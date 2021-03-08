5d08a9417d04526e110d3bd48eab676f54f5862f
Nathan Stormer <nate.stormer@knledg.com>
2021-03-08 12:32:22 -0700

    Adds interaction for mobile nav and no-js alert

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+// Wrap our logic in a Self-Invoking Anonymous Function
+// This prevents our vars and funcs to leak into global scope
+(function () {
+  // A function that removes JS disabled banner if this
+  // funcation can execute (meaning JS is enabled by client)
+  const testForJS = () => {
+    // 1. Get the body DOM element
+    //  Note: getElementsByTagName returns a list of nodes (ie: an Array)
+    //        Therefore, we need to get the first element in that array to access the DOM node
+    const body = document.getElementsByTagName("body")[0];
+
+    // 2. Remove the `no-js` class name from the body element
+    //    Our CSS rules will hide the notification banner if .no-js is removed
+    body.classList.remove("no-js");
+  };
+
+  // Logic for mobile menu drawer
+  const navToggle = () => {
+    // 1. Get DOM elements for menu container, hamburger button, and close button
+    const menu = document.querySelector(".js-nav");
+    const hamburgerBtn = document.querySelector(".js-hamburger");
+    const closeBtn = document.querySelector(".js-nav-close");
+    const activeClass = "nav-main--mobile-show";
+
+    // if there isn't a menu, return out of the function
+    if (!menu) {
+      return;
+    }
+
+    // 2. Toggle mobile nav visibility class to show/hide menu
+    const toggleNav = () => {
+      menu.classList.toggle(activeClass);
+    };
+
+    // 3. Listen for click event on hamburder and close buttons if they exist
+    if (hamburgerBtn) {
+      hamburgerBtn.addEventListener("click", toggleNav);
+    }
+
+    if (closeBtn) {
+      closeBtn.addEventListener("click", toggleNav);
+    }
+  };
+
+  };
+
+  // All function initialization goes here
+  const init = () => {
+    testForJS();
+    navToggle();
+  };
+
+  // Call initialization function
+  init();
+})();
